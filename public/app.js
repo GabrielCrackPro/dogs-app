c9a1315305b353ba10817dbcf126d14e9638ef11
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2021-08-21 19:49:30 +0200

    Project finished 🎉

--- public/app.js
+++ public/app.js
@@ -19,12 +19,18 @@ dogContainer.innerHTML = `
 <h3 class="text-capitalize">${dog.name}</h3>
 <img src="${dog.image}" alt="dog-img">
 <div class="similar">
- <h3 class="text-capitalize text-center">Similar Dogs</h3>
+ <h3 class="text-capitalize text-center">Similar Images</h3>
 </div>
-<button class="btn btn-sm btn-dark">Learn More</button>
+<button class="btn btn-sm btn-dark more-btn">Learn More <i class="fas fa-paw"></i></button>
 `
+const moreBtn = document.querySelector('.more-btn')
 const similarDogsContainer = document.querySelector('.similar')
-for(let i = 0; i <= 6; i++){
+
+moreBtn.addEventListener('click', () => {
+window.open(`http://wikipedia.org/wiki/${data.message.split("/")[4]}`)
+})
+
+for(let i = 0; i <= 13; i++){
 similarDogsContainer.innerHTML += `
 <img src="${randomBreedImages.message[i]}" alt="dog-img">
 `
